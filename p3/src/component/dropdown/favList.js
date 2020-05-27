f5c12d8d0e7e898a448e99c1535d03ffa61dc951
Oscar Nunez <noscar07@gmail.com>
2020-05-27 18:52:34 -0400

    working on showing list of favorite

---
+++ p3/src/component/dropdown/favList.js
@@ -0,0 +1,23 @@
+const Favdrivers =[
+    {
+        id:"1",
+        name: "Oscar Nunez",
+        Unit: "304",
+    },
+    {
+        id:"2",
+        name:"Raul Nino",
+        unit:"403"
+    },
+    {
+        id:"3",
+        name:"Nick",
+        unit:"305"
+    },
+    {
+        id:"4",
+        name:"ezi",
+        unit:"954"
+    }
+];
+export default Favdrivers;
--- p3/src/component/dropdown/list.js
+++ p3/src/component/dropdown/list.js
@@ -1,42 +1,89 @@
 import React from 'react';
 import "./style.css";
+import Favdrivers from './favList'
 
-function Favitems(props) {
-    return <li>{props.children} <button>Delete</button></li>;
-}
+//attempting to add into my list
+const Favlist=()=>{
+    const [value,setValue] = React.useState('');
+    const [list,setList] = React.useState(Favdrivers);
+    
+    const handleInputChange =(e) =>{
+        setValue(e.target.value)};
 
-class Favlist extends React.Component{
-    constructor(props){
-        super(props);
-        this.state={
-            currentList:"",
-            list:[]
+    const handleInputsubmit = (e)=>{
+        if(value){
+            setList(list.concat(value));
         }
-    }
+        setValue('');
+        e.preventDefault();
+    };
 
-    handleInputChange=(e)=>{
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
+    return<div>
+<ul>
+    {Favdrivers.map(item=>(
+        <li key={item.id}>
+        <div>{item.id}</div>
+        <div>{item.name}</div>
+        <div>{item.unit}</div>
+        </li>
+    ))};
+</ul>
+<form onSubmit={handleInputsubmit}>
+    <input type="text" value={value} onChange={handleInputChange}/>
+    <button type="submit">Add Driver</button>
+</form>
+</div>
+};
 
-    addFavorite=()=>{
-        const currentList = this.state.list;
-        currentList.push(this.state.currentList);
-        this.setState({
-            task:currentList
-        })
-    }
+//Able to display properties from Object.
+// const Favlist=()=>{
+// return<ul>
+//     {Favdrivers.map(item=>(
+//         <li key={item.id}>
+//         <div>{item.id}</div>
+//         <div>{item.name}</div>
+//         <div>{item.unit}</div>
+//         </li>
+//     ))};
+// </ul>
+// };
 
-    render() {
-        return <div>
-        <h1>Current favorites</h1>
-        <input name="Favlist" onChange={this.handleInputChange} value={this.state.currentTask} type="text" /> <button onClick={this.addTask}>Add friend</button>
-        <ul>
-            {this.state.list.map((list,index)=><Favitems key={`List-${index}`}>{list}</Favitems>)}
-        </ul>
-        </div>;
-    }
-}
+// function Favitems(props) {
+//     return <li>{props.children} <button>Delete</button></li>;
+// }
+
+// class Favlist extends React.Component{
+//     constructor(props){
+//         super(props);
+//         this.state={
+//             currentList:"",
+//             list:[]
+//         }
+//     }
+
+//     handleInputChange=(e)=>{
+//         this.setState({
+//             [e.target.name]:e.target.value
+//         })
+//     }
+
+//     addFavorite=()=>{
+//         const currentList = this.state.list;
+//         currentList.push(this.state.currentList);
+//         this.setState({
+//             task:currentList
+//         })
+//     }
+
+//     render() {
+//         return <div>
+//         <h1>Current favorites</h1>
+//         <input name="Favlist" onChange={this.handleInputChange} value={this.state.currentTask} type="text" /> <button onClick={this.addTask}>Add friend</button>
+//         <ul>
+//             {this.state.list.map((list,index)=><Favitems key={`List-${index}`}>{list}</Favitems>)}
+//         </ul>
+//         </div>;
+//     }
+// }
 
 export default Favlist;
