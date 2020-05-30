cc9d81732326d6620c057e3b47b30bad8f9a39b3
Eze <eze.largaespada@gmail.com>
2020-05-30 14:17:41 -0400

    page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import LandingPage from './LandingPage';
 import * as serviceWorker from './serviceWorker';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
