ce1c33987f4adbe3492d0263906585afd9cc9ea4
Oscar Nunez <noscar07@gmail.com>
2020-05-30 10:29:01 -0400

    making sure i backed up

---
+++ p3/src/component/Parked/Parked.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Table (){
+    return (
+        <div>
+           <div class="card" q>
+  <div class="card-header">
+    Currently Parked
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+            </div>
+        </div>
+    
+
+    );
+}
+export default Table;
--- p3/src/pages/Dashboardpage.js
+++ p3/src/pages/Dashboardpage.js
@@ -1,11 +1,19 @@
 import React from "react";
 import Title from "../component/header/Header";
 import Favlist from "../component/dropdown/list.js";
+import Banner from "../component/Banner/Banner.js";
+import Buttons from "../component/Buttons/Buttons.js";
+import Navbar from "../component/Navbar/Navbar.js";
+import Table from "../component/Parked/Parked.js";
 
 function Dashboardpage(){
     return<div> <h2>This is where we are going to display our dashboard components</h2>
-    <Title/>
-    <Favlist/>
+    {/* <Title/>
+    <Favlist/> */}
+    <Banner/>
+    <Buttons/>
+    <Navbar/>
+    <Table/>
     </div>
 };
 
