d0791c5a620fb875bf85da90209e9e118fe5a5cc
Ren <raulnino24@gmail.com>
2020-05-30 15:06:18 -0400

    added landing page

---
+++ src/component/Authorization.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from '../component/Login';
+import Signup from '../component/signup';
+
+
+
+const Authorization=()=>{
+    return <div>
+    <Login></Login>
+    <Signup></Signup>
+    
+    </div>
+}
+
+
+export default Authorization;
